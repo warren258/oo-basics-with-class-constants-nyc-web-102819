e60bfb331bb560a9c0c0d69f2573b1ae368feb07
Warren McDonald <warrenjmac@mail.com>
2019-10-30 17:49:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,7 @@ class Shoe
 BRANDS = []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include(brand)
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
